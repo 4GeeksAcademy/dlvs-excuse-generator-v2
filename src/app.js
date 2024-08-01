86d555f1edea9082ca5c4ba6161f0327578237a7
Danny Valdivia <dluis.valdivia@gmail.com>
2024-08-01 22:43:17 +0000

    update to excuse generator

--- src/app.js
+++ src/app.js
@@ -6,6 +6,27 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
+  //inner;
   //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  //randomizing
+  let who0 = Math.floor(Math.random() * who.length);
+  let action0 = Math.floor(Math.random() * action.length);
+  let what0 = Math.floor(Math.random() * what.length);
+  let when0 = Math.floor(Math.random() * when.length);
+
+  //inserting into html
+  document.querySelector(
+    "#excuse"
+  ).innerHTML = `${who[who0]} ${action[action0]} ${what[what0]} ${when[when0]}`;
 };
